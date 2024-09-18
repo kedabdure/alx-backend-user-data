cb3d415cb7066a2da75b5bd7f797c53cf1484561
kedabudre <abdurehimk77@gmail.com>
2024-09-18 10:57:59 +0300

    create a class called Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Module of auth class
+"""
+from typing import List, TypeVar
+from flask import request
+from user import User
+
+
+class Auth:
+    """Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Decide whether a given URL path needs authentication
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Retrieve the Authorization header from an HTTP request
+        """
+        return None
+    
+    def current_user(self, request=None) -> User:
+        """Retrieve the current user based on the request.
+        """
+        return None
