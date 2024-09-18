bc3c43c2a8af87179b8294d65420bc4fdb57c348
kedabudre <abdurehimk77@gmail.com>
2024-09-18 18:20:19 +0200

    update the auth module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,8 @@ from typing import List, TypeVar
 from flask import request
 from user import User
 
+User = TypeVar('User')
+
 
 class Auth:
     """Auth class
@@ -13,12 +15,12 @@ class Auth:
         """Decide whether a given URL path needs authentication
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Retrieve the Authorization header from an HTTP request
         """
         return None
-    
+
     def current_user(self, request=None) -> User:
         """Retrieve the current user based on the request.
         """
