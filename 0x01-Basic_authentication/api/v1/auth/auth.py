5b11d8f0944f22472d1ecc22f1e78a731df9415f
kedabudre <abdurehimk77@gmail.com>
2024-09-18 18:43:18 +0200

    update the auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python3
-""" Module of auth class
+""" Module of Authentication
 """
-from typing import List, TypeVar
 from flask import request
-from user import User
-
-User = TypeVar('User')
+from typing import List, TypeVar
+from models.user import User
 
 
 class Auth:
-    """Auth class
+    """Class to manage the API authentication
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Decide whether a given URL path needs authentication
+        """ Method for validating if endpoint requires auth
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Retrieve the Authorization header from an HTTP request
-        """
-        return None
+        """ Method that handles authorization header """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> User:
-        """Retrieve the current user based on the request.
-        """
+        """ Validates current user """
         return None
