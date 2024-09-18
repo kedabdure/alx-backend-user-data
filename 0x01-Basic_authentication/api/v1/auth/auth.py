6c4aecb03a6309316f3c9aafea2fb9306aacee9e
kedabudre <abdurehimk77@gmail.com>
2024-09-18 18:50:19 +0200

    update the auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,32 +13,32 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method for validating if endpoint requires auth
         """
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
+        # if path is None or excluded_paths is None or excluded_paths == []:
+        #     return True
 
-        l_path = len(path)
-        if l_path == 0:
-            return True
+        # l_path = len(path)
+        # if l_path == 0:
+        #     return True
 
-        slash_path = True if path[l_path - 1] == '/' else False
+        # slash_path = True if path[l_path - 1] == '/' else False
 
-        tmp_path = path
-        if not slash_path:
-            tmp_path += '/'
+        # tmp_path = path
+        # if not slash_path:
+        #     tmp_path += '/'
 
-        for exc in excluded_paths:
-            l_exc = len(exc)
-            if l_exc == 0:
-                continue
+        # for exc in excluded_paths:
+        #     l_exc = len(exc)
+        #     if l_exc == 0:
+        #         continue
 
-            if exc[l_exc - 1] != '*':
-                if tmp_path == exc:
-                    return False
-            else:
-                if exc[:-1] == path[:l_exc - 1]:
-                    return False
+        #     if exc[l_exc - 1] != '*':
+        #         if tmp_path == exc:
+        #             return False
+        #     else:
+        #         if exc[:-1] == path[:l_exc - 1]:
+        #             return False
 
-        return True
+        return False
 
     def authorization_header(self, request=None) -> str:
         """ Method that handles authorization header """
