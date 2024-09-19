78cb1241cc36e7c2c91acaac21b8d402635c24d9
kedabudre <abdurehimk77@gmail.com>
2024-09-19 23:08:43 +0200

    update the pycode style for the a session_auth module

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ import uuid
 class SessionAuth(Auth):
     """Class for session authentication"""
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """Creates a session ID for a user ID"""
         if user_id is None or not isinstance(user_id, str):
