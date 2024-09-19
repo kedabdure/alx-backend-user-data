c8b68302ab84d60dac40e6da443fb3da7d2d9a43
kedabudre <abdurehimk77@gmail.com>
2024-09-19 10:12:10 +0300

    update the base_auth for filter user capability

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
 from typing import Tuple
 
 
@@ -43,3 +44,22 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
+        """returns the User instance based on his email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        user = User.search({'email': user_email})
+        if not user or len(user) == 0:
+            return None
+
+        user = user[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
