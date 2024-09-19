7cbdb59f9a00f9c5cc1ccc82c9f7c6fb176ff287
kedabudre <abdurehimk77@gmail.com>
2024-09-19 22:38:11 +0200

    upate the app.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """Class for session authentication"""
+    pass
